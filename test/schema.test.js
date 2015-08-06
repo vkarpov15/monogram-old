8457c215403f50958f4f059a0c85626dafb51d58
Valeri Karpov <valkar207@gmail.com>
2015-08-06 10:48:46 -0400

    basic schema

---
+++ test/schema.test.js
@@ -0,0 +1,45 @@
+var assert = require('assert');
+var Schema = require('../lib/schema');
+
+describe('schema', function() {
+  it('compiles paths', function() {
+    var schema = new Schema({
+      test: 'Number',
+      nested: {
+        a: {
+          $type: 'Number'
+        }
+      }
+    });
+
+    schema.compile();
+
+    assert.deepEqual(schema._paths, {
+      test: { $type: 'Number' },
+      nested: { $type: 'nested' },
+      'nested.a': { $type: 'Number' }
+    });
+  });
+
+  it('handles arrays', function() {
+    var schema = new Schema({
+      test: 'Number',
+      arrMixed: [],
+      arrPlain: ['Number'],
+      arrNested: [['Number']]
+    });
+
+    schema.compile();
+
+    assert.deepEqual(schema._paths, {
+      'test': { $type: 'Number' },
+      'arrMixed': { $type: 'array' },
+      'arrMixed.$': { $type: 'mixed' },
+      'arrPlain': { $type: 'array' },
+      'arrPlain.$': { $type: 'Number' },
+      'arrNested': { $type: 'array' },
+      'arrNested.$': { $type: 'array' },
+      'arrNested.$.$': { $type: 'Number' }
+    });
+  });
+});
