1933649438c39ba58e3c16a97e8f713160d6d944
Valeri Karpov <valkar207@gmail.com>
2015-09-10 09:34:47 -0400

    use composition instead of koa-compose for return values

--- test/examples.test.js
+++ test/examples.test.js
@@ -32,7 +32,7 @@ describe('connecting and querying', function() {
     });
   });
 
-  it('save middleware', function(done) {
+  it('$save middleware', function(done) {
     co(function*() {
       var db = yield monogram('mongodb://localhost:27017');
       var schema = new monogram.Schema({});
@@ -48,6 +48,28 @@ describe('connecting and querying', function() {
       var t = new Test({ _id: 5 });
 
       yield t.$save();
+
+      done();
+    }).catch(function(error) {
+      done(error);
+    });
+  });
+
+  it('query middleware', function(done) {
+    co(function*() {
+      var db = yield monogram('mongodb://localhost:27017');
+      var schema = new monogram.Schema({});
+      schema.middleware('find', function*(next) {
+        var docs = yield next;
+        assert.equal(docs.length, 0);
+        docs.push({ _id: 'fakedoc' });
+        return docs;
+      });
+      var Test = db.model({ schema: schema, collection: 'test3' });
+
+      var docs = yield Test.find({});
+
+      assert.deepEqual(docs, [{ _id: 'fakedoc' }]);
 
       done();
     }).catch(function(error) {
