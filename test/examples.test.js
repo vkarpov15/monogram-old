c12963d80050c08bfff483584a86e7ac826eda2c
Valeri Karpov <valkar207@gmail.com>
2015-09-15 09:36:51 -0400

    rearrange examples and make middleware work for custom document and query methods

--- test/examples.test.js
+++ test/examples.test.js
@@ -33,62 +33,11 @@ describe('connecting and querying', function() {
     });
   });
 
-  it('$save middleware', function(done) {
-    co(function*() {
-      let db = yield monogram('mongodb://localhost:27017');
-      let schema = new monogram.Schema({});
-      schema.middleware('$save', function*(next) {
-        assert.ok(this.$isNew());
-        yield next;
-        assert.ok(!this.$isNew());
-      });
-      let Test = db.model({ schema: schema, collection: 'test2' });
-
-      yield Test.deleteMany({});
-
-      let t = new Test({ _id: 5 });
-
-      yield t.$save();
-
-      done();
-    }).catch(function(error) {
-      done(error);
-    });
-  });
-
-  it('query middleware', function(done) {
-    co(function*() {
-      let db = yield monogram('mongodb://localhost:27017');
-      let schema = new monogram.Schema({});
-      schema.middleware('find', function*(next) {
-        var docs = yield next;
-        assert.equal(docs.length, 1);
-        docs.push({ _id: 'fakedoc' });
-        return docs;
-      });
-      schema.middleware('find', function*(next) {
-        var docs = yield next;
-        assert.equal(docs.length, 0);
-        docs.push({ _id: 'fakedoc' });
-        return docs;
-      });
-      let Test = db.model({ schema: schema, collection: 'test3' });
-
-      let docs = yield Test.find({});
-
-      assert.deepEqual(docs, [{ _id: 'fakedoc' }, { _id: 'fakedoc' }]);
-
-      done();
-    }).catch(function(error) {
-      done(error);
-    });
-  });
-
   it('query builder', function(done) {
     co(function*() {
       let db = yield monogram('mongodb://localhost:27017');
       let schema = new monogram.Schema({});
-      let Test = db.model({ schema: schema, collection: 'test4' });
+      let Test = db.model({ schema: schema, collection: 'test' });
 
       yield Test.deleteMany({});
 
@@ -110,101 +59,199 @@ describe('connecting and querying', function() {
     });
   });
 
-  it('custom document methods', function(done) {
-    co(function*() {
-      let db = yield monogram('mongodb://localhost:27017');
-      let schema = new monogram.Schema({});
+  describe('custom methods', function() {
+    it('custom document methods', function(done) {
+      co(function*() {
+        let db = yield monogram('mongodb://localhost:27017');
+        let schema = new monogram.Schema({});
 
-      schema.method('document', '$validate', function() {
-        throw new Error('validation error!');
+        schema.method('document', '$validate', function() {
+          throw new Error('validation error!');
+        });
+
+        schema.middleware('$save', function*(next) {
+          yield this.$validate();
+          yield next;
+        });
+
+        let Test = db.model({ schema: schema, collection: 'test' });
+
+        yield Test.deleteMany({});
+
+        let t = new Test({ _id: 5 });
+
+        try {
+          yield t.$save();
+          assert.ok(false);
+        } catch(err) {
+          assert.equal(err.toString(), 'Error: validation error!');
+        }
+
+        let count = yield Test.count({});
+
+        assert.equal(count, 0);
+
+        done();
+      }).catch(function(error) {
+        done(error);
       });
+    });
 
-      schema.middleware('$save', function*(next) {
-        yield this.$validate();
-        yield next;
+    it('custom query methods', function(done) {
+      co(function*() {
+        let db = yield monogram('mongodb://localhost:27017');
+        let schema = new monogram.Schema({});
+
+        schema.method('query', 'checkVisible', function() {
+          this.find({ isVisible: true });
+          return this;
+        });
+
+        let Test = db.model({ schema: schema, collection: 'test' });
+
+        yield Test.deleteMany({});
+
+        yield [
+          new Test({ _id: 1, isVisible: true }).$save(),
+          new Test({ _id: 2, isVisible: false }).$save()
+        ];
+
+        let docs = yield Test.find({}).checkVisible();
+
+        assert.equal(docs.length, 1);
+
+        assert.equal(docs[0]._id, 1);
+
+        done();
+      }).catch(function(error) {
+        done(error);
       });
+    });
 
-      let Test = db.model({ schema: schema, collection: 'test5' });
+    it('custom model methods', function(done) {
+      co(function*() {
+        let db = yield monogram('mongodb://localhost:27017');
+        let schema = new monogram.Schema({});
 
-      yield Test.deleteMany({});
+        schema.method('model', 'findVisible', function() {
+          return this.find({ isVisible: true });
+        });
 
-      let t = new Test({ _id: 5 });
+        let Test = db.model({ schema: schema, collection: 'test' });
 
-      try {
+        yield Test.deleteMany({});
+
+        yield [
+          new Test({ _id: 1, isVisible: true }).$save(),
+          new Test({ _id: 2, isVisible: false }).$save()
+        ];
+
+        let docs = yield Test.findVisible();
+
+        assert.equal(docs.length, 1);
+
+        assert.equal(docs[0]._id, 1);
+
+        done();
+      }).catch(function(error) {
+        done(error);
+      });
+    });
+  });
+
+  describe('middleware', function() {
+    it('$save middleware', function(done) {
+      co(function*() {
+        let db = yield monogram('mongodb://localhost:27017');
+        let schema = new monogram.Schema({});
+        schema.middleware('$save', function*(next) {
+          assert.ok(this.$isNew());
+          yield next;
+          assert.ok(!this.$isNew());
+        });
+        let Test = db.model({ schema: schema, collection: 'test' });
+
+        yield Test.deleteMany({});
+
+        let t = new Test({ _id: 5 });
+
         yield t.$save();
-        assert.ok(false);
-      } catch(err) {
-        assert.equal(err.toString(), 'Error: validation error!');
-      }
 
-      let count = yield Test.count({});
-
-      assert.equal(count, 0);
-
-      done();
-    }).catch(function(error) {
-      done(error);
-    });
-  });
-
-  it('custom query methods', function(done) {
-    co(function*() {
-      let db = yield monogram('mongodb://localhost:27017');
-      let schema = new monogram.Schema({});
-
-      schema.method('query', 'checkVisible', function() {
-        this.find({ isVisible: true });
-        return this;
+        done();
+      }).catch(function(error) {
+        done(error);
       });
-
-      let Test = db.model({ schema: schema, collection: 'test6' });
-
-      yield Test.deleteMany({});
-
-      yield [
-        new Test({ _id: 1, isVisible: true }).$save(),
-        new Test({ _id: 2, isVisible: false }).$save()
-      ];
-
-      let docs = yield Test.find({}).checkVisible();
-
-      assert.equal(docs.length, 1);
-
-      assert.equal(docs[0]._id, 1);
-
-      done();
-    }).catch(function(error) {
-      done(error);
     });
-  });
 
-  it('custom model methods', function(done) {
-    co(function*() {
-      let db = yield monogram('mongodb://localhost:27017');
-      let schema = new monogram.Schema({});
+    it('query middleware', function(done) {
+      co(function*() {
+        let db = yield monogram('mongodb://localhost:27017');
+        let schema = new monogram.Schema({});
+        schema.middleware('find', function*(next) {
+          var docs = yield next;
+          assert.equal(docs.length, 1);
+          docs.push({ _id: 'fakedoc' });
+          return docs;
+        });
+        schema.middleware('find', function*(next) {
+          var docs = yield next;
+          assert.equal(docs.length, 0);
+          docs.push({ _id: 'fakedoc' });
+          return docs;
+        });
+        let Test = db.model({ schema: schema, collection: 'test' });
 
-      schema.method('model', 'findVisible', function() {
-        return this.find({ isVisible: true });
+        yield Test.deleteMany({});
+
+        let docs = yield Test.find({});
+
+        assert.deepEqual(docs, [{ _id: 'fakedoc' }, { _id: 'fakedoc' }]);
+
+        done();
+      }).catch(function(error) {
+        done(error);
       });
+    });
 
-      let Test = db.model({ schema: schema, collection: 'test7' });
+    it('for custom methods', function(done) {
+      co(function*() {
+        let db = yield monogram('mongodb://localhost:27017');
+        let schema = new monogram.Schema({});
 
-      yield Test.deleteMany({});
+        schema.method('document', '$validate', function() {
+          return true;
+        });
 
-      yield [
-        new Test({ _id: 1, isVisible: true }).$save(),
-        new Test({ _id: 2, isVisible: false }).$save()
-      ];
+        schema.middleware('$validate', function*(next) {
+          throw new Error('pre-validation error!');
+        });
 
-      let docs = yield Test.findVisible();
+        schema.middleware('$save', function*(next) {
+          yield this.$validate();
+          yield next;
+        });
 
-      assert.equal(docs.length, 1);
+        let Test = db.model({ schema: schema, collection: 'test' });
 
-      assert.equal(docs[0]._id, 1);
+        yield Test.deleteMany({});
 
-      done();
-    }).catch(function(error) {
-      done(error);
+        let t = new Test({ _id: 5 });
+
+        try {
+          yield t.$save();
+          assert.ok(false);
+        } catch(err) {
+          assert.equal(err.toString(), 'Error: pre-validation error!');
+        }
+
+        let count = yield Test.count({});
+
+        assert.equal(count, 0);
+
+        done();
+      }).catch(function(error) {
+        done(error);
+      });
     });
   });
 });
